0d939eefa18e738ed571e7bae70013277825b1fa
Marius Kaunietis <marius@mediapark.lt>
2017-08-21 19:02:31 +0300

    add login ui

--- Testio/app/src/main/java/lt/marius/testio/LoginActivity.kt
+++ Testio/app/src/main/java/lt/marius/testio/LoginActivity.kt
@@ -2,6 +2,9 @@ package lt.marius.testio
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.WindowManager
+import kotlinx.android.synthetic.main.activity_login.*
 
 /**
  * A login screen that offers login via email/password.
@@ -9,7 +12,21 @@ import android.support.v7.app.AppCompatActivity
 class LoginActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
+		window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+		                WindowManager.LayoutParams.FLAG_FULLSCREEN)
 		setContentView(R.layout.activity_login)
+
+
+		loginFormLogin.setOnClickListener {
+			validateAndRun()
+		}
+
+	}
+
+	private fun validateAndRun() {
+		loginProgressContainer.visibility = View.VISIBLE
+		loginForm.visibility = View.GONE
+		loginProgressMessage.setText(R.string.login_progress_message_login)
 	}
 }
 
