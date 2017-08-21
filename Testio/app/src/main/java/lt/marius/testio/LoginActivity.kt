e965733fcb826c7d19e292d907e858711cf0c0be
Marius Kaunietis <mariuskaunietis@gmail.com>
2017-08-21 15:39:50 +0300

    initial project setup, started addding login layout

---
+++ Testio/app/src/main/java/lt/marius/testio/LoginActivity.kt
@@ -0,0 +1,15 @@
+package lt.marius.testio
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * A login screen that offers login via email/password.
+ */
+class LoginActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_login)
+	}
+}
+
