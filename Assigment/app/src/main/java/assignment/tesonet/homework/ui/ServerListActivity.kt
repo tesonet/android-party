5f922b3766c99d2458df4a003662ce9b1d064e45
deiauk <deividas717@gmail.com>
2018-06-27 23:39:43 +0300

    init commit

--- Assigment/app/src/main/java/assignment/tesonet/homework/ui/ServerListActivity.kt
+++ Assigment/app/src/main/java/assignment/tesonet/homework/ui/ServerListActivity.kt
@@ -23,6 +23,7 @@ import android.support.v4.content.ContextCompat
 import android.support.v7.widget.DividerItemDecoration
 import android.view.View
 
+
 class ServerListActivity : AppCompatActivity() {
 
     private lateinit var viewModel: ServerActivityViewModel
