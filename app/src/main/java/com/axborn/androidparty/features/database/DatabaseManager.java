3e104254c9a1c97f73bd4b501da39efaf65f31c3
Ignas Vaitekunas <axborn@gmail.com>
2018-07-25 21:18:22 +0300

    Basic structure. Added Login and Feed activities

---
+++ app/src/main/java/com/axborn/androidparty/features/database/DatabaseManager.java
@@ -0,0 +1,79 @@
+package com.axborn.androidparty.features.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.axborn.androidparty.features.utils.User;
+
+public class DatabaseManager extends SQLiteOpenHelper {
+
+    private final static int    DB_VERSION = 10;
+
+    public DatabaseManager(Context context) {
+        super(context, "myApp.db", null,DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String query = "create table logins (userId Integer primary key autoincrement, "+
+                " username text, password text)";
+        sqLiteDatabase.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        try{
+            System.out.println("UPGRADE DB oldVersion="+oldVersion+" - newVersion="+newVersion);
+            onCreate(sqLiteDatabase);
+            if (oldVersion<10){
+                String query = "create table logins (userId Integer primary key autoincrement, "+
+                        " username text, password text)";
+                sqLiteDatabase.execSQL(query);
+            }
+        }
+        catch (Exception e){e.printStackTrace();}
+    }
+
+    @Override
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // super.onDowngrade(db, oldVersion, newVersion);
+        System.out.println("DOWNGRADE DB oldVersion="+oldVersion+" - newVersion="+newVersion);
+    }
+
+    public User insertUser (User queryValues){
+        SQLiteDatabase database = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("username", queryValues.username);
+        values.put("password", queryValues.password);
+        queryValues.userId=database.insert("logins", null, values);
+        database.close();
+        return queryValues;
+    }
+
+    public int updateUserPassword (User queryValues){
+        SQLiteDatabase database = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("username", queryValues.username);
+        values.put("password", queryValues.password);
+        queryValues.userId=database.insert("logins", null, values);
+        database.close();
+        return database.update("logins", values, "userId = ?", new String[] {String.valueOf(queryValues.userId)});
+    }
+
+    public User getUser (String username){
+        String query = "Select userId, password from logins where username ='"+username+"'";
+        User myUser = new User(0,username,"");
+        SQLiteDatabase database = this.getReadableDatabase();
+        Cursor cursor = database.rawQuery(query, null);
+        if (cursor.moveToFirst()){
+            do {
+                myUser.userId=cursor.getLong(0);
+                myUser.password=cursor.getString(1);
+            } while (cursor.moveToNext());
+        }
+        return myUser;
+    }
+}
---
+++ app/src/main/java/com/axborn/androidparty/features/feed/FeedActivity.java
@@ -0,0 +1,33 @@
+package com.axborn.androidparty.features.feed;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+import com.axborn.androidparty.R;
+
+public class FeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView tokenView = (TextView) findViewById(R.id.tokenView);
+                tokenView.setText("Actual token");
+            }
+        });
+
+    }
+
+}
