2184507e7521606722b6b32494e222e203811ec7
Erikas Bulevičius <erikas@ito.lt>
2018-10-11 21:33:48 +0300

    Initial commit
    Created project for android studio.

---
+++ app/src/main/java/lt/bulevicius/tessonetapp/MainActivity.java
@@ -0,0 +1,13 @@
+package lt.bulevicius.tessonetapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
