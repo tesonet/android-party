29393ac21c624160d498d50f8e8ed242aa3011c1
Mindaugas Varkalys <varkalys.mindaugas@gmail.com>
2018-11-18 18:00:26 +0100

    Created Login screen design

---
+++ app/src/main/java/com/tesonet/testio/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.tesonet.testio
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
