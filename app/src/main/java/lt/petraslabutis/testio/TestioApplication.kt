2d95ea56cee25c073a6cda8a7d67f0db10165505
Petras Labutis <labutispetras@gmail.com>
2019-02-17 17:40:36 +0200

    Add Realm configuration

--- app/src/main/java/lt/petraslabutis/testio/TestioApplication.kt
+++ app/src/main/java/lt/petraslabutis/testio/TestioApplication.kt
@@ -2,18 +2,28 @@ package lt.petraslabutis.testio
 
 import android.app.Application
 import io.realm.Realm
+import io.realm.RealmConfiguration
 import lt.petraslabutis.testio.dagger.*
 class TestioApplication: Application() {
 
     companion object {
         @JvmStatic
         lateinit var applicationComponent: ApplicationComponent
+        private const val CURRENT_REALM_SCHEMA_VERSION = 1L
     }
 
     override fun onCreate() {
         super.onCreate()
 
         Realm.init(this)
+
+        val realmConfiguration = RealmConfiguration
+            .Builder()
+            .schemaVersion(CURRENT_REALM_SCHEMA_VERSION)
+            .deleteRealmIfMigrationNeeded()
+            .build()
+
+        Realm.setDefaultConfiguration(realmConfiguration)
 
         applicationComponent = DaggerApplicationComponent.builder()
             .appModule(AppModule(applicationContext))
