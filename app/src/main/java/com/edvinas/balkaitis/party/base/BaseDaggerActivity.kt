960dc2d8a6c70076f13e96cceb7c7ddce097552b
edvbal <edvinas.balkaitis@hermes-europe.co.uk>
2019-04-04 20:00:01 +0100

    Create BaseDaggerActivity

---
+++ app/src/main/java/com/edvinas/balkaitis/party/base/BaseDaggerActivity.kt
@@ -0,0 +1,15 @@
+package com.edvinas.balkaitis.party.base
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import dagger.android.support.DaggerAppCompatActivity
+
+abstract class BaseDaggerActivity : DaggerAppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(getLayoutId())
+    }
+
+    @LayoutRes
+    protected abstract fun getLayoutId(): Int
+}
