58aa49fa867c5c3231930121d5c94470deb01db3
edvbal <edvinas.balkaitis@hermes-europe.co.uk>
2019-04-04 20:12:33 +0100

    Create BaseDaggerFragment

---
+++ app/src/main/java/com/edvinas/balkaitis/party/base/BaseDaggerFragment.kt
@@ -0,0 +1,19 @@
+package com.edvinas.balkaitis.party.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import dagger.android.support.DaggerFragment
+
+abstract class BaseDaggerFragment : DaggerFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(getLayoutId(), container, false)
+
+    @LayoutRes
+    abstract fun getLayoutId(): Int
+}
