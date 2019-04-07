7310088aa1ead22de1b5fda77e3bf5dbe2075c2f
edvbal <edvinas.balkaitis@hermes-europe.co.uk>
2019-04-07 21:30:29 +0100

    Launch LoginFragment in LoginActivity#onCreate

--- app/src/main/java/com/edvinas/balkaitis/party/login/LoginActivity.kt
+++ app/src/main/java/com/edvinas/balkaitis/party/login/LoginActivity.kt
@@ -1,8 +1,17 @@
 package com.edvinas.balkaitis.party.login
 
+import android.os.Bundle
 import com.edvinas.balkaitis.party.R
 import com.edvinas.balkaitis.party.base.BaseDaggerActivity
+import com.edvinas.balkaitis.party.utils.extensions.replaceFragment
 
 class LoginActivity : BaseDaggerActivity() {
-    override fun getLayoutId()= R.layout.activity_login
+    override fun getLayoutId() = R.layout.activity_login
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (savedInstanceState == null) {
+            replaceFragment(LoginFragment.newInstance())
+        }
+    }
 }
