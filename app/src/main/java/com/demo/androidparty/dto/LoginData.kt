b65fa6edaa475c455f1f296ec61ef5baf9a869db
Deividas <deividas.aukstakalnis@deeper.eu>
2020-07-31 10:19:59 +0300

    added @Keep annotation for dto classes

--- app/src/main/java/com/demo/androidparty/dto/LoginData.kt
+++ app/src/main/java/com/demo/androidparty/dto/LoginData.kt
@@ -1,3 +1,6 @@
 package com.demo.androidparty.dto
 
+import androidx.annotation.Keep
+
+@Keep
 data class LoginData(val username: String, val password: String)
--- app/src/main/java/com/demo/androidparty/dto/TokenData.kt
+++ app/src/main/java/com/demo/androidparty/dto/TokenData.kt
@@ -1,3 +1,6 @@
 package com.demo.androidparty.dto
 
+import androidx.annotation.Keep
+
+@Keep
 data class TokenData(val token: String)
