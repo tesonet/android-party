87d21769d58170dd158563af265a4b6125e880ed
Jonas Simonaitis <dzonas.sim@gmail.com>
2021-08-30 20:41:37 +0300

    Create login fragment and viewmodel

---
+++ app/src/main/java/com/tesonet/testio/ui/login/LoginFragment.kt
@@ -0,0 +1,24 @@
+package com.tesonet.testio.ui.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.constraintlayout.motion.widget.MotionLayout
+import com.tesonet.testio.R
+import dagger.android.support.DaggerFragment
+import javax.inject.Inject
+
+class LoginFragment : DaggerFragment() {
+
+    @Inject
+    lateinit var viewModel: LoginViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.login_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
