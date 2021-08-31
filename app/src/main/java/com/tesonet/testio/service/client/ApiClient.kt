4cb086fdc1809a10e8ebcc91be520b8b6a87db9c
Jonas Simonaitis <dzonas.sim@gmail.com>
2021-08-31 23:04:29 +0300

    Create fetchServer end-point

--- app/src/main/java/com/tesonet/testio/service/client/ApiClient.kt
+++ app/src/main/java/com/tesonet/testio/service/client/ApiClient.kt
@@ -1,20 +1,21 @@
 package com.tesonet.testio.service.client
 
+import com.tesonet.testio.service.data.server.Server
 import com.tesonet.testio.service.data.token.Token
 import com.tesonet.testio.service.data.user.RequestUser
-import io.reactivex.Observable
 import io.reactivex.Single
-import retrofit2.http.Body
-import retrofit2.http.Headers
-import retrofit2.http.POST
+import retrofit2.http.*
 
 interface ApiClient {
 
     @Headers(CONTENT_TYPE)
     @POST("v1/tokens")
-    fun requestUserToken(@Body requestUser: RequestUser): Observable<Token>
+    fun requestUserToken(@Body requestUser: RequestUser): Single<Token>
+
+    @GET("v1/servers")
+    fun fetchServers(@Header("Authorization") token: String): Single<List<Server>>
 
     companion object {
-        const val CONTENT_TYPE: String = "Content-Type: application/json"
+        private const val CONTENT_TYPE: String = "Content-Type: application/json"
     }
 }
