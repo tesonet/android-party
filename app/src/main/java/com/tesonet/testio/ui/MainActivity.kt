2b3fa46fbd3d8310f665fa877353261571db8ca3
Jonas Simonaitis <dzonas.sim@gmail.com>
2021-08-31 23:05:10 +0300

    Navigate to fragment according to sign in value from shared prefs

--- app/src/main/java/com/tesonet/testio/ui/MainActivity.kt
+++ app/src/main/java/com/tesonet/testio/ui/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.tesonet.testio.ui
 
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import com.tesonet.testio.R
 import dagger.android.AndroidInjection
 import dagger.android.AndroidInjector
@@ -18,9 +20,27 @@ class MainActivity : AppCompatActivity(), HasAndroidInjector {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        checkIfUserIsLoggedIn()
     }
 
     override fun androidInjector(): AndroidInjector<Any> {
         return androidInjector
+    }
+
+    private fun checkIfUserIsLoggedIn() {
+        this.getSharedPreferences(SHARED_PREFERENCES_FILE_NAME, Context.MODE_PRIVATE).let {
+            val isLoggedIn = it.getBoolean(LOG_IN_STATE, false)
+            if (!isLoggedIn) {
+                findNavController(R.id.nav_host_fragment).navigate(R.id.loginFragment)
+            } else {
+                findNavController(R.id.nav_host_fragment).navigate(R.id.loadingFragment) // TODO Change with server list fragment
+            }
+        }
+    }
+
+    companion object {
+
+        const val SHARED_PREFERENCES_FILE_NAME: String = "testio_shared"
+        const val LOG_IN_STATE: String = "log_in_state"
     }
 }
