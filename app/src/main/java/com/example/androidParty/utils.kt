da3e61b20b351e0e96d2004307c1ffc7954e1caa
Mai Abdelsalm <mai.abdelsalam@door2door.io>
2022-01-11 01:43:04 +0100

    More cleaning for the whole project

--- app/src/main/java/com/example/androidParty/utils.kt
+++ app/src/main/java/com/example/androidParty/utils.kt
@@ -8,9 +8,3 @@ fun View.enable(enabled: Boolean) {
   isEnabled = enabled
   alpha = if (enabled) 1f else 0.5f
 }
-//
-//
-//@BindingAdapter("distance")
-//fun bindDistance(textView: AppCompatTextView, distance: String) {
-//  textView.text = "$distance km "
-//}
