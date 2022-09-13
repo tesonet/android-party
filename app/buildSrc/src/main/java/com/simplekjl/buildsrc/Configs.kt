458096b3042d0dacc017b50f796cfc20c5c0403b
Jose Luis Crisostomo <jl.cs606@gmail.com>
2022-09-13 13:34:36 +0200

    initial setup with gradle files and project structure

---
+++ app/buildSrc/src/main/java/com/simplekjl/buildsrc/Configs.kt
@@ -0,0 +1,9 @@
+package com.simplekjl.buildsrc
+
+object Configs {
+    const val compilation = 32
+    const val applicationId = "com.simplekjl.Stores"
+    const val minSdk = 26
+    const val target = 32
+    const val testRunner = "androidx.test.runner.AndroidJUnitRunner"
+}
